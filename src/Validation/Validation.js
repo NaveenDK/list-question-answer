7ea92d04f07bc93c11e6a055d9608e83a1d9ffda
naveendk <naveenkolambage@gmail.com>
2019-02-03 02:27:56 +1200

    more maintanable validation

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 const validation = (props) => {
+   let validationMessage = 'Text long enough';
+
+   if (props.inputLength <=5){
+       validationMessage = 'Text too short';
+   }
+
     return (
         <div>
-            {
-                props.inputLength > 5 ?
-                <p>Text Long enough</p>:
-                <p> Text too short! </p>
-            }
-            
-            
+                <p>{validationMessage}</p>
         </div>
     )
 };
